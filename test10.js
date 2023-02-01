d1b1439d1371eadd64621fd9adb3e59f960b6720
shafi8099 <rehanafridi267@gmail.com>
2023-02-01 10:46:32 -0500

    intial commit

---
+++ test10.js
@@ -0,0 +1,30 @@
+function insert(num) {
+
+    document.form.display.value = document.form.display.value + num;
+
+  }
+
+  function equal() {
+
+    var exp = document.form.display.value;
+
+    if (exp) {
+
+      document.form.display.value = exp;
+
+    }
+
+  }
+
+  function backspace() {
+
+    var exp = document.form.display.value;
+
+    document.form.display.value = exp.substring(0, exp.length - 2);
+
+  }
+
+  function remove() {
+    
+    document.form.display.value = "";
+ }
